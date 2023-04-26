b009047c09224f06f7d8dc54821d62fb2c7cb0b0
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2023-04-26 14:51:46 -0300

    inicio refatoracao

--- js/modais.js
+++ js/modais.js
@@ -1,11 +1,23 @@
-const textoModais = {
+const textoModaisSolucoes = {
 
     1: 'Aqui entra a solução proposta para o problema 1',
     2:'Aqui entra a solução proposta para o problema 2',
     3: 'Aqui entra a solução proposta para o problema 3'
 }
 
+const textoModaisAnalises = {
+
+    1: 'Aqui entra a analise da metodo 1',
+    2:'Aqui entra a analise da metodo 2',
+    3: 'Aqui entra a analise da metodo 3'
+}
+
 
 function showModal(modal){
-    $('.texto-modal').html(textoModais[modal]);
+    $('.texto-modal').html(textoModaisSolucoes[modal]);
+}
+
+
+function showModalAnalises(modal){
+    $('.texto-modal').html(textoModaisAnalises[modal]);
 }
