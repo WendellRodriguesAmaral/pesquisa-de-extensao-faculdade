5cf1abd2c7ef54059affea9eabacd7cbf0802d13
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2023-05-02 10:33:14 -0300

    add texto de detalhes sobre o robo

--- js/modais.js
+++ js/modais.js
@@ -7,7 +7,7 @@ const textoModaisSolucoes = {
 
 const textoModaisAnalises = {
 
-    1: ` <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur et ipsam impedit sit adipisci vel, voluptas laudantium perspiciatis exercitationem odit. Possimus ducimus quae, natus corporis illum eius distinctio. At, quia!</p>
+    1: ` <p class="pt-2">Foi desenvolvido um robô que tem como objetivo extrair as reclamações avaliadas de uma determinada empresa do site Reclame Aqui. O robô foi desenvolvido na linguagem Python, o mesmo utiliza a biblioteca "Selenium", que tem como objetivo permitir que o robô interaja com o navegador através dos seletores das páginas, para fazer a extração das informações das reclamações, foi utilizado a biblioteca "Beautiful Soup" que faz o Web Scraping da tela, extraindo o texto do seletor que for passado para ele e, para salvar as informações extraídas, foi utilizado a biblioteca "Pandas", para fazer a leitura do arquivo xlsx  que é onde tem os parâmetro utilizados pelo robô, que são, a URL da página da empresa no Reclame Aqui e o caminho onde será salvo o arquivo csv, também foi utilizado essa biblioteca, para salvar as informações extraídas da reclamação em um arquivo csv.</p>
     <h5>Veja o GIF:</h5>
     <div class="w-100 d-flex align-items-center justify-content-center">
         <img class="w-75 img-fluid rounded-3" src="./gifs/Gif robô em execução.gif" alt="GIF do robô em python rodando">
