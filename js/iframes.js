2045ed440c667f75b018ffe3cf9971bbd557f844
Wendell Rodrigues Amaral <wendellra@ciandt.com>
2023-05-01 21:30:43 -0300

    adição de imagens, responsividade, links, e 2 graficos

--- js/iframes.js
+++ js/iframes.js
@@ -1,17 +1,20 @@
 $('.if-1').click(function(e){
     $('.d-1').html('')
     $('.d-1').html(
-        '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/d22c1d10-748d-4489-876b-560746cd4c0b/page/TWJND" ></iframe>'
+        '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/a93727bb-09aa-4548-a911-a4dea7d66cca/page/NWmOD" ></iframe>'
 
     )
+    $('.texto-iframe').html("texto analise 1");
 })
 
 $('.if-2').click(function(e){
     $('.d-2').html('')
     $('.d-2').html(
-        '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/7cbd5259-888d-41ae-8e28-230a3ece0221/page/w1hND" ></iframe>'
+        '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/0819fce8-0a0e-41c4-8510-44ad5c86fd9c/page/cemOD"" ></iframe>'
 
     )
+    $('.texto-iframe').html("texto analise 2");
+
 })
 
 $('.if-3').click(function(e){
@@ -20,6 +23,8 @@ $('.if-3').click(function(e){
         '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/d22c1d10-748d-4489-876b-560746cd4c0b/page/TWJND" ></iframe>'
 
     )
+    $('.texto-iframe').html("texto analise 3");
+
 })
 
 $('.if-4').click(function(e){
@@ -27,5 +32,6 @@ $('.if-4').click(function(e){
     $('.d-4').html(
         '<iframe class="iframe m-auto img-fluid"  src="https://lookerstudio.google.com/embed/reporting/d22c1d10-748d-4489-876b-560746cd4c0b/page/TWJND" ></iframe>'
 
-    )
+        )
+        $('.texto-iframe').html("texto analise 4");
 })
